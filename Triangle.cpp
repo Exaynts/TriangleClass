9c9d2232a1f83b22484f0dc4348fee81da1c1f08
JustJam <exayntszhenya@gmail.com>
2025-04-15 11:42:01 +0300

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+// Êîíñòðóêòîð
 Triangle::Triangle(Point _v1, Point _v2, Point _v3, const char* ident) : v1(_v1), v2(_v2), v3(_v3) {
 	char buf[16];
 
@@ -15,7 +16,7 @@ Triangle::Triangle(Point _v1, Point _v2, Point _v3, const char* ident) : v1(_v1)
 	strcpy(objID, ident);
 	count++;
 
-	sprintf(buf, "Constructor %d", count);
+	sprintf(buf, "Òðåóãîëüíèê %d", count);
 
 	name = new char[strlen(buf) + 1];
 	strcpy(name, buf);
@@ -25,16 +26,16 @@ Triangle::Triangle(Point _v1, Point _v2, Point _v3, const char* ident) : v1(_v1)
 	c = sqrt((v1.x - v3.x) * (v1.x - v3.x) + (v1.y - v3.y) * (v1.y - v3.y));
 
 	cout << "Constructor_l for: " << objID
-		<< " (" << name << " ) " << endl;
+		<< " (" << name << " ) " << endl; // îòëàäî÷íûé âûâîä
 }
-
+// Êîíñòðóêòîð ïóñòîãî (íóëåâîãî) òðåóãîëüíèêà
 Triangle::Triangle(const char* ident) {
 	char buf[16];
 	objID = new char[strlen(ident) + 1];
 	strcpy(objID, ident);
 
 	count++;
-	sprintf(buf, "Constructor %d", count);
+	sprintf(buf, "Òðåóãîëüíèê %d", count);
 	name = new char[strlen(buf) + 1];
 
 	strcpy(name, buf);
@@ -42,26 +43,26 @@ Triangle::Triangle(const char* ident) {
 
 	cout << "Constructor_2 for: " << objID << " (" << name << ")" << endl;
 }
-
+// Äåñòðóêòîð
 Triangle::~Triangle() {
 	cout << "Destructor for: " << objID << endl;
 	delete[] objID;
 	delete[] name;
 }
 
-
+// Ïîêàçàòü îáúåêò
 void Triangle::Show() const {
 	cout << name << ":";
 	v1.Show(); v2.Show(); v3.Show();
 	cout << endl;
 }
-
-void Triangle::ShowSideAndArea() const {
+// Ïîêàçàòü ñòîðîíû è ïëîùàäü îáúåêòà
+void Triangle::ShowSideAndArea() const{
 	double p = (a + b + c) / 2;
 	double s = sqrt(p * (p - a) * (p - b) * (p - c));
 	cout << " • " << endl;
 	cout << name << ":";
-	cout.precision(4);
+	cout.precision(4); 
 	cout << " a= " << setw(5) << a;
 	cout << ". b= " << setw(5) << b;
 	cout << ". c= " << setw(5) << c;
