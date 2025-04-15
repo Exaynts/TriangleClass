8259f3d7be33586b6e529b240d9ff65b513ce0b2
JustJam <exayntszhenya@gmail.com>
2025-04-15 10:38:19 +0300

    Create Point.h

---
+++ Point.h
@@ -0,0 +1,14 @@
+#pragma once
+// Point.h
+#ifndef POINT_H
+#define POINT_H
+class Point {
+public:
+
+    Point(double _x = 0, double _y = 0) : x(_x), y(_y) {}
+
+    void Show() const;
+public:
+    double x, y;
+};
+#endif /* POINT_H */
